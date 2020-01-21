68684c58b14e3fa1e448d4de510567a49837b557
mikezalik <mike.zalik@gmail.com>
2020-01-21 11:00:30 -0800

    Adds method for testing whether number is square and returns boolean.

--- app/src/main/java/com/mikezalik/numbershapes/MainActivity.java
+++ app/src/main/java/com/mikezalik/numbershapes/MainActivity.java
@@ -9,9 +9,20 @@ public class MainActivity extends AppCompatActivity {
     class Number {
         int number;
 
+        public boolean isSquare() {
+            double squareRoot = Math.sqrt(number);
+
+            if (squareRoot == Math.floor(squareRoot)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
         public boolean isTriangular() {
             int x = 1;
             int triangularNumber = 1;
+
             while (triangularNumber < number) {
                 x++;
                 triangularNumber = triangularNumber + x;
